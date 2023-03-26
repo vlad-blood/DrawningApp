6d73716997ff2dcae638682a3d5a2dab49c8d10c
vlad-blood <128965945+vlad-blood@users.noreply.github.com>
2023-03-26 13:45:59 +0300

    dobavili izmenenie razmera kisti

--- test.py
+++ test.py
@@ -7,17 +7,20 @@ class DrawApp():
         self.window.geometry('600x600')
         self.brush_size = 2
         self.c = tk.Canvas(self.window, width=450, height=450, bg="grey")
-        self.c.pack()
+        self.right_frame = tk.Frame()
+        self.right_frame.pack(side=tk.LEFT)
+        self.c.pack(side=tk.LEFT)
         self.c.bind('<B1-Motion>', self.draw)
         self.c.bind('<B2-Motion>', self.clean)
+        self.buttons()
         self.window.mainloop()
 
     def draw(self,event):
-        brush_size = 5
-        self.c.create_oval(event.x - brush_size,
-                      event.y - brush_size,
-                      event.x + brush_size,
-                      event.y + brush_size, fill='black')
+        # self.brush_size = 5
+        self.c.create_oval(event.x - self.brush_size,
+                      event.y - self.brush_size,
+                      event.x + self.brush_size,
+                      event.y + self.brush_size, fill='black')
 
     def clean(self,event):
         brush_size = 200
@@ -25,6 +28,15 @@ class DrawApp():
                       event.y - brush_size,
                       event.x + brush_size,
                       event.y + brush_size, fill='grey', outline='grey')
+    def buttons(self):
+        for i in range(1,6):
+            tk.Button(self.right_frame,text=f"{i}",bg="black",fg="white",
+                      width=8,height=4,command=lambda f=i: self.change_brash_size(f)).pack(side=tk.TOP)
+
+    def change_brash_size(self,size):
+        self.brush_size = size * 5
+        print("work",size)
+
 
 
 if __name__ == '__main__':
